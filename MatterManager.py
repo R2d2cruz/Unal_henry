c8a31e40b4519c8af4cdeeac747fc51c9a1c3de7
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2020-06-02 11:12:55 -0500

    add MatterManager

---
+++ MatterManager.py
@@ -0,0 +1,10 @@
+from Student import Student
+from Matter import Matter
+
+
+class MatterManager:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
---
+++ __main__.py
@@ -0,0 +1,6 @@
+
+from MatterManager import MatterManager
+
+if __main__ == "__main__":
+    matterManager = MatterManager()
+    matterManager.update()
