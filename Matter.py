e49b78944863b9d68c6d743bfdc5f5596e99a9e5
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2020-06-02 11:02:00 -0500

    añadir metodos para verificar la inscripcion

--- Matter.py
+++ Matter.py
@@ -2,9 +2,11 @@
 
 
 class Matter:
-    def __init__(self, name: str, days: Dict):
+    def __init__(self, name: str, days: Dict, maxStudents: int):
         self.__days = days
         self.name = name
+        self.__numStudents = 0
+        self.__maxStudents = maxStudents
 
 
     @property
@@ -13,3 +15,13 @@ class Matter:
 
     def getHoursByDay(self, day: str):
         return self.__days.get(day)
+
+    @property
+    def numStudents(self):
+        return self.__students
+
+    def addStudent(self):
+        if self.__numStudents < self.__maxStudents:
+            self.__numStudents += 1
+            return True
+        return False
--- Student.py
+++ Student.py
@@ -12,11 +12,25 @@ class Student:
 
     def addMatter(self, matter: Matter):
         matterName = matter.name
+        daysPass = {}
         for day in matter.days:
             for hour in matter.getHoursByDay(day):
+                # se revisa cada hora de cada dia que tiene la materia
                 if self.schedule.get(day).get(hour) is None:
+                    # si no hay materias ahi, añada la materia
                     self.schedule[day][hour] = matterName
+                    # en caso de que por alguna razon no se pueda guarda los dias y horas que ha guardado esa materia
+                    daysPass[day] = hour
                 else:
+                    # si la materia tiene conflicto entonces eliminar todos las horas donde esta
+                    deleteAllScheduleMatter(daysPass)
                     return False
-
+        # devuelve true si pudo inscribir la materia
         return True
+
+
+    def deleteAllScheduleMatter(self, daysPass: Dict):
+        for day in daysPass.keys():
+            for hour in daysPass.get(day):
+                del self.schedule[day][hour]
+                # elimina las apariciones de la materia mencionada
