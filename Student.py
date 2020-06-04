f26ab68d0ed0f3c245c261f6ebf1898b82e1aa5b
krenfonseca <66313646+krenfonseca@users.noreply.github.com>
2020-06-04 10:49:05 -0500

    Name added

--- Student.py
+++ Student.py
@@ -8,6 +8,7 @@ Student:
 Authors:
         Carlos Arturo Cruz Useche - 1001048369
         Diego Alejandro Campos Méndez - 1005753729
+        Karen Fonseca Sánchez - 1020825523
 
 
 """
--- VisualInterface.py
+++ VisualInterface.py
@@ -5,6 +5,7 @@ VisualInterface:
 Authors:
         Carlos Arturo Cruz Useche - 1001048369
         Diego Alejandro Campos Méndez - 1005753729
+        Karen Fonseca Sánchez - 1020825523
 
 """
 
