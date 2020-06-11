1c323c363caf9fa9de4f672d1dc807f1ef89553e
Henry Salomón <hessuarezlo@unal.edu.co>
2020-06-11 07:43:23 -0500

    Interfaz gráfica

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -69,6 +70,48 @@ class GUI(App):
     title = 'Horario UN'
     def build(self):
         return kv
+=======
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MyGrid(Widget):
+    days = ObjectProperty(None)
+    name = ObjectProperty(None)
+    numbstud = ObjectProperty(None)
+    maxstud = ObjectProperty(None)
+    mattcode = ObjectProperty(None)
+    owl = ObjectProperty(None)
+    credits = ObjectProperty(None)
+    slack = ObjectProperty(None)
+
+    def ChrgMatter(self):
+        print("Días:", self.days.text, "Nombre:", self.name.text, "Número de estudiantes:", self.numbstud.text,"Número máximo de estudiantes:",
+              self.maxstud.text,"Código de la materia:", self.mattcode.text,"Profesor:", self.owl.text,"Valor en créditos:", self.credits.text,"Holgura en sobrecupos:", self.slack.text,)
+
+    def NewMatter(self):
+        self.days.text = ""
+        self.name.text = ""
+        self.numbstud.text = ""
+        self.maxstud.text = ""
+        self.mattcode.text = ""
+        self.owl.text = ""
+        self.credits.text = ""
+        self.slack.text = ""
+
+
+class GUI(App):
+    def build(self):
+        return MyGrid()
+>>>>>>> Interfaz gráfica
 
 
 if __name__ == "__main__":
