61f0add7d74b766fb06bf1207e67746a2241bc45
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2020-07-09 07:45:33 -0500

    change setup to cx-freeze

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-from setuptools import setup
+# from setuptools import setup
+#
+# setup(
+#     name='Horarios UNAL',
+#     version='1.0.0',
+#     author='Carlos Arturo Cruz Useche\nDiego Alejandro Campos Méndez\nKaren Fonseca Sánchez\nHenry Salomón Suárez López',
+#     scripts=['__main__.py'],
+#     # packing=[],
+#     install_requires=['pyqueue==2.0.0', 'kivy==1.11.1', 'grid==0.7.1']
+# )
+import cx_Freeze
 
-setup(
+executables = [
+    cx_Freeze.Executable("__main__.py")
+]
+
+cx_Freeze.setup(
     name='Horarios UNAL',
     version='1.0.0',
-    author='Carlos Arturo Cruz Useche\nDiego Alejandro Campos Méndez\nKaren Fonseca Sánchez\nHenry Salomón Suárez López',
-    scripts=['__main__.py'],
-    # packing=[],
-    install_requires=['pyqueue==2.0.0', 'kivy==1.11.1', 'grid==0.7.1']
+    options={"build_exe": {"packages": ["pyqueue", "kivy", "grid"], "include_files": [
+        'ProgramObjects/'
+    ]}},
+    executables=executables
 )
+
+# python setup.py build
